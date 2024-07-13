a50a1dfde2a70288be06361db58d72a4716f512f
Raul-TC <raultc.9819@gmail.com>
2024-07-13 01:13:45 -0600

    assets

---
+++ app/assets/fonts.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line camelcase
+import { Montserrat, Luckiest_Guy, Balsamiq_Sans } from 'next/font/google'
+
+export const montserrat = Montserrat({
+    weight: ['400', '800'],
+    subsets: ['latin']
+})
+
+export const luckiestGuy = Luckiest_Guy({
+    subsets: ['latin'],
+    weight: ['400']
+})
+export const balsamiqSans = Balsamiq_Sans({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
