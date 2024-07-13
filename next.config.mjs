59ee5c2427173c61036aa2ccd577e39095918be0
Raul-TC <raultc.9819@gmail.com>
2024-07-13 01:11:22 -0600

    basic config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.fortniteapi.io',
+        pathname: '**'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'bg-header': '#18181c',
+        'bg-body': '#101014',
+        'yellowForrnite': '#F7FF19'
+      }
+
     },
   },
   plugins: [],
