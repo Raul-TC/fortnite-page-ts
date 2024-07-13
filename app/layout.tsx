46a13d11437563b923262b85dbc43701599f55a7
Raul-TC <raultc.9819@gmail.com>
2024-07-13 02:21:58 -0600

    fix className layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
     <html lang="es">
       <body className={`${balsamiqSans.className} bg-bg-body text-white pb-[80px]`}>
         <Header />
-        <div className='w-[95%] max-w-[1440px] m-auto  mt-[100px] min-h-[95vh]'>
+        <div className='w-[95%] max-w-[1440px] m-auto  mt-[100px] md:min-h-[95vh]'>
           {children}
         </div>
         <footer className='w-full text-center fixed bottom-0 h-[40px] mt-[50px] bg-bg-header flex items-center justify-center z-[20] gap-1'>Made by  <Link href='https://github.com/Raul-TC' className='text-red-400'>Raul-TC 💖</Link> </footer>
