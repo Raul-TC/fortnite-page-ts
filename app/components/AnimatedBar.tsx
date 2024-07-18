eb0135b5e0a574933925ad5b9964ac0caa51c2f6
Raul-TC <raultc.9819@gmail.com>
2024-07-18 03:44:52 -0600

    fix type

--- app/components/AnimatedBar.tsx
+++ app/components/AnimatedBar.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+interface CustomStyle extends React.CSSProperties {
+    '--progress-width'?: string;
+}
+
 export const AnimatedBar = ({ color, data, height, index }: { color: string, data: number, height: string, index: string }) => {
+    // @ts-ignore
+    const barStyle: CustomStyle = {
+        '--progress-width': `${data}%`,
+    };
     return (
-        <div className={`bg-${color}-500 h-${height} rounded-full absolute z-${index} progress-bar-animated progress-bar`} style={{ '--progress-width': `${data}%` }}></div>
+        <div className={`bg-${color}-500 h-${height} rounded-full absolute z-${index} progress-bar-animated progress-bar`} style={barStyle}></div>
     )
 }
