8c0763196fa8df8b849ed5cb95f711a3fa9e694e
Raul-TC <raultc.9819@gmail.com>
2024-08-19 11:29:21 -0600

    change Image to img

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -18,28 +18,20 @@ interface ImageSliderProps {
 }
 export const ImageSlider = ({ displayName, arrayImages, price, isItem, bg }: ImageSliderProps) => {
     const { optimizedImages } = useImageSlider({ arrayImages, displayName });
+
     return (
         <>
             {optimizedImages.map((el, index) => (
-                // <Image
-                //     key={`${el.key}`}
-                //     width={300}
-                //     height={300}
-                //     quality={70}
-                //     src={el.background}
-                //     alt={`${el.materialInstance}`}
-                //     className={`transition-all duration-500 ${el.isActive ? 'w-full h-full ' : 'hidden'} rounded-md `} />
-
-                bg !== ''
+                bg
                     ? (
                         <div key={`${index}_${displayName}`} className=' overflow-hidden rounded-md'>
 
-                            <Image src={bg} alt='' className='absolute top-0 bottom-0 left-0 right-0 -z-0 h-full w-full' width={450} height={450} quality={60} />
-                            <Image src={el.url} alt={`${el.materialInstance}`} className={`${el.isActive ? '  relative z-10' : ' hidden'} w-full h-full`} width={450} height={450} quality={60} />
+                            <img src={bg} alt='' className='absolute top-0 bottom-0 left-0 right-0 -z-0 h-full w-full' width={450} height={450} />
+                            <img src={el.url} alt={`${el.materialInstance}`} className={`${el.isActive ? '  relative z-10' : ' hidden'} w-full h-full`} width={450} height={450} />
 
                         </div>
                     )
-                    : <Image key={`${displayName}_${index}`} src={el.background} alt={`${el.materialInstance}_`} className={`${el.isActive ? 'w-full' : 'hidden'} rounded-md `} width={450} height={450} quality={60} />
+                    : <img key={`${displayName}_${index}`} src={el.background} alt={`${el.materialInstance}_`} className={`${el.isActive ? 'w-full' : 'hidden'} rounded-md `} width={450} height={450} />
 
 
             ))}
